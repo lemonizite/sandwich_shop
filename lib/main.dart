4787ec0309d5f5c7d8aba115bde575f5155a74af
lemonizite <lemonizite@gmail.com>
2025-10-17 13:06:13 +0100

    created app class as stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
